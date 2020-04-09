79afb323f5ec4c75776871cc3f4189cbd0abaf3e
natxsglez <44679189+natxsglez@users.noreply.github.com>
2020-04-09 13:17:35 -0500

    Update ClkRedu.v

--- Practica_3/ClkRedu.v
+++ Practica_3/ClkRedu.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Nataly Salazar
 // 
 // Create Date:    10:50:57 04/09/2020 
 // Design Name: 
@@ -24,7 +24,8 @@ module ClkRedu(
     output reg clkRedu,
 	 integer MAX_MHZ
     );
-reg [31:0] conteo; //se definen 25 bits para el conteo porque 2^25 = 33Millones
+	 
+reg [31:0] conteo;
 always @(posedge clk, posedge reset)
 	begin
 		if(reset)
@@ -37,8 +38,15 @@ always @(posedge clk, posedge reset)
 				conteo <= conteo + 1;
 				if(conteo == MAX_MHZ) //contar hasta 25 millones para hacer Toggleo cada 0.5s
 					begin
-						clkRedu <= clkRedu + 1; //Togglea la salida
-						conteo <=0; //reinicia la cuenta
+						case(clkRedu)
+							9 : begin
+									clkRedu <= 0;
+								end
+							default : begin
+									clkRedu <= clkRedu + 1;
+								end
+							endcase
+						conteo <= 0; //reinicia la cuenta
 					end
 			end
 	end
