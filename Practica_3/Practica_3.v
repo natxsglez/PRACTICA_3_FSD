7ed2cd73efa4331f9ceda5738684ba3eb353ef33
natxsglez <44679189+natxsglez@users.noreply.github.com>
2020-04-09 13:26:51 -0500

    Update Practica_3.v

--- Practica_3/Practica_3.v
+++ Practica_3/Practica_3.v
@@ -1,23 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: Nataly Salazar
-// 
-// Create Date:    10:27:45 04/09/2020 
-// Design Name: 
-// Module Name:    Practica_3 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 module Practica_3(
 	input clk,
 	output [7:0] Num
@@ -29,10 +9,10 @@ wire clkReduSecsD;
 
 reg [3:0]T;
 
-ClkRedu ClkDec(.clk(clk), .reset(reset), .clkRedu(clkReduDec), . MAX_MHZ(2_500_000));
-ClkRedu ClkSecsU(.clk(clk), .reset(reset), .clkRedu(clkReduSecsU), . MAX_MHZ(25_000_000));
-ClkRedu ClkSecsD(.clk(clk), .reset(reset), .clkRedu(clkReduSecsD), . MAX_MHZ(250_000_000));
-ClkRedu ClkMins(.clk(clk), .reset(reset), .clkRedu(clkReduMins), . MAX_MHZ(1_500_000_000));
+ClkRedu ClkDec(.clk(clk), .reset(reset), .clkRedu(clkReduDec), .MAX_MHZ(2_500_000), .MAX_COUNT(9));
+ClkRedu ClkSecsU(.clk(clk), .reset(reset), .clkRedu(clkReduSecsU), .MAX_MHZ(25_000_000), .MAX_COUNT(9));
+ClkRedu ClkSecsD(.clk(clk), .reset(reset), .clkRedu(clkReduSecsD), .MAX_MHZ(250_000_000), MAX_COUNT(5));
+ClkRedu ClkMins(.clk(clk), .reset(reset), .clkRedu(clkReduMins), .MAX_MHZ(1_500_000_000), .MAX_COUNT(9));
 
 Decodificador Decodificador0(.clkReduDec(clkReduDec), .clkReduMins(clkReduMins), .clkReduSecsU(clkReduSecsU), . clkReduSecsD(clkReduSecsD));
 
