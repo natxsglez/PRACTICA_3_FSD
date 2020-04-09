e50359b86abcb8e35c3104e13a5a417bb4733e8b
natxsglez <44679189+natxsglez@users.noreply.github.com>
2020-04-09 13:19:33 -0500

    Update Practica_3.v
    Adding the Decoder

--- Practica_3/Practica_3.v
+++ Practica_3/Practica_3.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Nataly Salazar
 // 
 // Create Date:    10:27:45 04/09/2020 
 // Design Name: 
@@ -22,12 +22,18 @@ module Practica_3(
 	input clk,
 	output [7:0] Num
     );
-wire clkReduCen;
+wire clkReduDec;
 wire clkReduMins;
-wire clkReduSecs;
+wire clkReduSecsU;
+wire clkReduSecsD;
 
-ClkRedu ClkCen(.clk(clk), .reset(reset), .clkRedu(clkReduCen), . MAX_MHZ(2_500_000));
-ClkRedu ClkSecs(.clk(clk), .reset(reset), .clkRedu(clkReduSecs), . MAX_MHZ(25_000_000));
-ClkRedu ClkMins(.clk(clk), .reset(reset), .clkRedu(clkRedu), . MAX_MHZ(1_500_000_000));
+reg [3:0]T;
+
+ClkRedu ClkDec(.clk(clk), .reset(reset), .clkRedu(clkReduDec), . MAX_MHZ(2_500_000));
+ClkRedu ClkSecsU(.clk(clk), .reset(reset), .clkRedu(clkReduSecsU), . MAX_MHZ(25_000_000));
+ClkRedu ClkSecsD(.clk(clk), .reset(reset), .clkRedu(clkReduSecsD), . MAX_MHZ(250_000_000));
+ClkRedu ClkMins(.clk(clk), .reset(reset), .clkRedu(clkReduMins), . MAX_MHZ(1_500_000_000));
+
+Decodificador Decodificador0(.clkReduDec(clkReduDec), .clkReduMins(clkReduMins), .clkReduSecsU(clkReduSecsU), . clkReduSecsD(clkReduSecsD));
 
 endmodule
